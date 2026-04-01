de0d4fade85c2c142b35cea0cbb3ce8d7642fee3
Philipp A. <flying-sheep@web.de>
2026-04-01 12:33:01 +0200

    unify

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from '@vscode/test-cli'
 export default defineConfig({
 	files: 'src/test/**/*.test.ts',
 	mocha: {
-		require: 'esbuild-register', // todo unify with bundler
+		require: '@swc-node/register',
 	},
 })
